94adac230eeeeb2bf05f22d16cf55f903545f084
itsRoze <elewis9989@gmail.com>
2023-05-09 02:35:13 -0400

    fixes for prod deployment

--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -3,7 +3,6 @@ import { NextjsSite, type StackContext } from "sst/constructs";
 export function WebStack({ stack }: StackContext) {
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
-    buildCommand: "pnpm run build",
   });
 
   stack.addOutputs({
