bd8385135851e084ec6a2ef36ce42d8a7152599e
itsRoze <elewis9989@gmail.com>
2023-05-10 19:23:56 -0400

    add Prisma

--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -4,6 +4,9 @@ import { NextjsSite } from "sst/constructs";
 export function WebStack({ stack }: StackContext) {
   const site = new NextjsSite(stack, "modal-web", {
     path: "apps/web",
+    environment: {
+      DATABASE_URL: process.env.DATABASE_URL!,
+    },
   });
 
   stack.addOutputs({
