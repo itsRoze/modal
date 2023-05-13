8624207297b43b5e1bcdacf6149fecfa94a003ad
itsRoze <elewis9989@gmail.com>
2023-05-13 16:10:26 -0400

    add icons

---
+++ apps/web/src/components/layouts/commerical/CommercialLayout.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+
+interface ICommericalLayout {
+  children: React.ReactNode;
+}
+
+const CommercialLayout: React.FC<ICommericalLayout> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Modal | Task Manager</title>
+        <meta
+          name="description"
+          content="Modal | The Last Task Manager You'll Ever Use"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default CommercialLayout;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { api } from "@/utils/api";
+import { type NextPageWithLayout } from "./_app";
+import CommercialLayout from "@/components/layouts/commerical/CommercialLayout";
 
-const Home = () => {
+const Home: NextPageWithLayout = () => {
   // const { data, isLoading } = api.example.hello.useQuery({ text: "Hello!!" });
   const { data, isLoading } = api.example.getAll.useQuery();
   if (isLoading) return <div>Loading...</div>;
   if (!isLoading && !data) return <div>Something went wrong</div>;
   return <div className="text-xl text-red-300">{data.count}</div>;
+};
+
+Home.getLayout = (page) => {
+  return <CommercialLayout>{page}</CommercialLayout>;
 };
 
 export default Home;
