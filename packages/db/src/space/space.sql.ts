702f40f4ca2da062b8e6cb1fa5c46d534ab1cad2
itsRoze <elewis9989@gmail.com>
2023-06-06 22:51:01 -0400

    create space model

---
+++ packages/db/src/space/space.sql.ts
@@ -0,0 +1,9 @@
+import { mysqlTable, varchar } from "drizzle-orm/mysql-core";
+
+import { id, timestamps } from "../utils/sql";
+
+export const space = mysqlTable("space", {
+  ...id,
+  ...timestamps,
+  name: varchar("name", { length: 255 }).notNull(),
+});
--- packages/db/src/utils/sql.ts
+++ packages/db/src/utils/sql.ts
@@ -1,5 +1,12 @@
 import { sql } from "drizzle-orm";
-import { timestamp } from "drizzle-orm/mysql-core";
+import { char, timestamp } from "drizzle-orm/mysql-core";
+
+export { createId } from "@paralleldrive/cuid2";
+
+export const cuid = (name: string) => char(name, { length: 24 });
+export const id = {
+  id: cuid("id").notNull(),
+};
 
 export const timestamps = {
   timeCreated: timestamp("time_created", {
