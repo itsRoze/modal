77c2f98e151e88dd12fe530fe7c2153f2f7a43c5
itsRoze <elewis9989@gmail.com>
2023-08-20 00:44:20 -0400

    test

--- apps/web/src/pages/api/auth/login/issue.ts
+++ apps/web/src/pages/api/auth/login/issue.ts
@@ -24,68 +24,11 @@ const ratelimit = new Ratelimit({
   analytics: true,
 });
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
   if (req.method !== "POST") res.status(404).json({ error: "Not found" });
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { email } =
-    typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-
-  if (!email || typeof email !== "string") {
-    return res.status(400).json({
-      error: "Invalid input",
-    });
-  }
-
-  try {
-    // Use a constant string to limit all requests with a single ratelimit
-    const { success } = await ratelimit.limit("login_issue");
-    if (!success) {
-      throw new Error("Too many requests");
-    }
-
-    const user = await fromEmail(email);
-    if (!user) throw new Error("No user found");
-    const key = await auth.useKey("email", email, null);
-
-    // delete previous tokens
-    await deleteTokenByUserId(user.id);
-    // generate new token
-    const otp = otpToken();
-    // save token
-    await createToken({
-      userId: user.id,
-      token: otp.token,
-      expires: otp.expires,
-    });
-
-    if (process.env.NODE_ENV === "development") {
-      console.log(otp);
-    } else {
-      await sendTokenEmail({
-        token: otp.token,
-        userEmail: email,
-        fromEmail: "no-reply@account.usemodal.com",
-      });
-    }
-
-    res.status(200).json({ message: "OTP sent", userId: key.userId });
-    console.log("RES JSON", res.json);
-  } catch (error) {
-    Sentry.captureException(error);
-    if (error instanceof LuciaError) {
-      if (error.message === "AUTH_INVALID_KEY_ID")
-        res.status(400).json({ error: "No user found" });
-    } else if (error instanceof Error) {
-      res.status(400).json({ error: error.message });
-    } else {
-      res.status(400).json({
-        error: "Unknown error occurred",
-      });
-    }
-    console.log("RES JSON", res.json);
-  }
+  res.status(200).json({ userId: "123" });
 }
