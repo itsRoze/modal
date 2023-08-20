3f0e2f948d925ce5578221fc2bb3f1a365e1a4ef
itsRoze <elewis9989@gmail.com>
2023-08-20 13:43:06 -0400

    test

--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import { type GetServerSideProps, type GetServerSidePropsContext } from "next";
-import { useRouter } from "next/router";
 import EmailForm from "@/components/forms/emailForm";
 import CommercialLayout from "@/components/layouts/commerical/CommercialLayout";
-import TokenForm from "@/components/tokenform";
 import { auth } from "@modal/auth";
-import * as Sentry from "@sentry/nextjs";
 import { motion } from "framer-motion";
-import { ZodError, z } from "zod";
 
 import { type NextPageWithLayout } from "./_app";
 
 const Login: NextPageWithLayout = () => {
   const [userId, setUserId] = useState<string | null>(null);
-  const [token, setToken] = useState("");
+
+  const onSubmit = async (formData: { email: string }) => {
+    console.log("SUBMIT", formData.email);
+    try {
+      const response = await fetch("/api/auth/login/issue", {
+        method: "POST",
+        body: JSON.stringify({ email: formData.email }),
+      });
+
+      const data = await response.text();
+      console.log("RES DATA", data);
+
+      if (!response.ok) {
+        throw new Error(data ?? "Something went wrong");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const variants = {
     hidden: { opacity: 0, x: -200, y: 0 },
@@ -37,116 +51,10 @@ const Login: NextPageWithLayout = () => {
       </section>
       <section className="flex w-full flex-col items-center space-y-4">
         <div className="my-10 rounded-lg bg-white py-2 shadow-lg md:p-10">
-          {!userId ? (
-            <LoginEmailForm setUserId={setUserId} />
-          ) : (
-            <LoginTokenForm otp={token} setOtp={setToken} userId={userId} />
-          )}
+          {!userId ? <EmailForm onSubmit={onSubmit} /> : null}
         </div>
       </section>
     </motion.article>
-  );
-};
-
-type ResponseData = {
-  userId?: string;
-  error?: string;
-};
-
-const LoginEmailForm = ({
-  setUserId,
-}: {
-  setUserId: React.Dispatch<React.SetStateAction<string | null>>;
-}) => {
-  const [error, setError] = useState<string | null>(null);
-
-  const onSubmit = async (formData: { email: string }) => {
-    console.log("SUBMIT", formData.email);
-    try {
-      const response = await fetch("/api/auth/login/issue", {
-        method: "POST",
-        body: JSON.stringify({ email: formData.email }),
-      });
-
-      const data = await response.text();
-      console.log("RES DATA", data);
-
-      if (!response.ok) {
-        throw new Error(data ?? "Something went wrong");
-      }
-
-      setUserId(data);
-    } catch (error) {
-      Sentry.captureException(error);
-      if (error instanceof Error) setError(error.message);
-      else setError("Something went wrong");
-    }
-  };
-
-  return <EmailForm onSubmit={onSubmit} error={error} />;
-};
-
-const LoginTokenForm = ({
-  otp,
-  setOtp,
-  userId,
-}: {
-  otp: string;
-  setOtp: React.Dispatch<React.SetStateAction<string>>;
-  userId: string;
-}) => {
-  const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
-  const formSchema = z.object({
-    otp: z.string().length(8),
-  });
-
-  const variants = {
-    hidden: { opacity: 0, x: 100, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      formSchema.parse({ otp });
-      const response = await fetch("/api/auth/login/validate", {
-        method: "POST",
-        body: JSON.stringify({ token: otp, userId }),
-      });
-      if (response.redirected) return router.push(response.url);
-
-      const data = (await response.json()) as {
-        error: string;
-      };
-
-      throw new Error(data.error ?? "Something went wrong");
-    } catch (error) {
-      if (error instanceof ZodError) {
-        setError(error.message);
-      }
-    }
-  };
-
-  return (
-    <motion.div
-      variants={variants} // Pass the variant object into Framer Motion
-      initial="hidden" // Set the initial state to variants.hidden
-      animate="enter" // Animated state to variants.enter
-      exit="exit" // Exit state (used later) to variants.exit
-      transition={{ type: "linear" }} // Set the transition to linear
-      className="flex flex-col items-center space-y-8 "
-    >
-      <TokenForm
-        setError={setError}
-        error={error}
-        onSubmit={onSubmit}
-        otp={otp}
-        setOtp={setOtp}
-      />
-    </motion.div>
   );
 };
 
