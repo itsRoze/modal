2603ca725724d998cbbc5b3ff346c4f30d3a3834
itsRoze <elewis9989@gmail.com>
2023-11-27 19:39:50 -0500

    rename custom domain

--- stacks/Dns.ts
+++ stacks/Dns.ts
@@ -2,11 +2,11 @@ import { type StackContext } from "sst/constructs";
 
 export function Dns({ stack }: StackContext) {
   if (stack.stage === "prod") {
-    return { domain: "usemodal.com" };
+    return { domain: "app.usemodal.com" };
   }
 
   if (stack.stage === "dev") {
-    return { domain: "dev.usemodal.com" };
+    return { domain: "dev-app.usemodal.com" };
   }
 
   return { domain: undefined };
