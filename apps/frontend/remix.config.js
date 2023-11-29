40e5bc30f0b0aace3a52d7773a86f44328c1802c
itsRoze <elewis9989@gmail.com>
2023-11-28 19:32:51 -0500

    setup remix with lucia again

--- apps/frontend/remix.config.js
+++ apps/frontend/remix.config.js
@@ -1,6 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    "lucia",
+    "lucia/middleware",
+    "lucia/polyfill/node",
+    "@lucia-auth/adapter-mysql", // adapter you're using
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
